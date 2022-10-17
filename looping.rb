287740704acfc61f613e04c819427c7b2010d8f2
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-10-17 22:50:14 +0300

    Completed Assignment

--- looping.rb
+++ looping.rb
@@ -1,8 +1,17 @@
 def happy_new_year
-  # your code here
+
+  count = 10
+  until count == 0
+    puts count
+    count -= 1
+  end
+  puts "Happy New Year!"
+
 end
 
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
+happy_new_year
+
+# No need to modify this code! Use this to implement the fizzbuzz_putser method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
@@ -17,8 +26,22 @@ end
 
 def fizzbuzz_printer
   # your code here
+  i = 1
+  until i == 101
+    puts fizzbuzz(i)
+    i += 1
+  end
 end
 
+
 def reverse_string(str)
-  # your code here
+  i = 0
+  string_array = []
+  while i < str.length
+    string_array.unshift(str[i])
+    i = i + 1
+  end
+  return string_array.join
 end
+
+reverse_string("cat")
